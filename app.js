6b4daeb9c42a3a6567d56954d476ca56e2528606
Sonu <sonualam1599@gmail.com>
2022-11-10 02:12:57 +0530

    updated app.js

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
+const https = require("https");
+const { dirname } = require("path");
 
 
 
@@ -15,12 +17,59 @@ app.get("/", function (req, res) {
 
 app.post("/", function (req, res) {
 
-    var firstName = req.body.fName;
-    var lastName = req.body.lName;
-    var email = req.body.email;
+    const firstName = req.body.fName;
+    const lastName = req.body.lName;
+    const email = req.body.email;
 
-    console.log(firstName, lastName, email);
+    // console.log(firstName, lastName, email);
+
+    //array of data
+    const data = {
+        members: [
+            {
+                email_address: email,
+                status: "subscribed",
+                merge_fields: {
+                    FNAME: firstName,
+                    LNAME: lastName
+                }
+            }
+        ]
+    };
+
+    const jsonData = JSON.stringify(data);
+
+    const url = "https://us9.api.mailchimp.com/3.0/lists/5049e743a5";
+
+    const options = {
+        method: "POST",
+        auth: "renon:33a8e6c94575eb8b7e950bb1618f53b9-us9"
+    }
+
+    const request = https.request(url, options, function (response) {
+
+
+
+        //if signup success or fail these would pop up
+        if (response.statusCode === 200) {
+            res.sendFile(__dirname + "/success.html");
+        } else {
+            res.sendFile(__dirname + "/failure.html");
+        }
+
+        response.on("data", function (data) {
+            console.log(JSON.parse(data))
+        })
+    });
+
+    request.write(jsonData);
+    request.end();
+
 });
+
+app.post("/failure", function (req, res) {
+    res.redirect("/");
+})
 
 app.listen(3000, function () {
     console.log("server is running on port 3000");
@@ -28,4 +77,7 @@ app.listen(3000, function () {
 
 
 //api key
-//97902edae83adb7b1c59415f4158bdd6-us9
+//33a8e6c94575eb8b7e950bb1618f53b9-us9
+
+// Unique id
+// 5049e743a5
